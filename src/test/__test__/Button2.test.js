53b64d5bcf11b1b870c00997e158953412d0091c
litao <736188281@qq.com>
2019-12-12 22:48:44 +0800

    test

---
+++ src/test/__test__/Button2.test.js
@@ -0,0 +1,13 @@
+import renderer from 'react-test-renderer'
+import React from 'react';
+import { Button } from '../Button2';
+
+describe('Button', () => {
+    it('matches snapshot', () => {
+        const component = renderer.create((
+            <Button />
+        ))
+        expect(component.toJSON()).toMatchSnapshot()
+    })
+})
+    
