3f5fc9eef47035060757e9f3ac7416e21f4ee3f4
litao <736188281@qq.com>
2019-12-12 23:57:11 +0800

    test

--- src/test/__test__/Button5.test.js
+++ src/test/__test__/Button5.test.js
@@ -1,12 +1,11 @@
 import renderer from 'react-test-renderer'
 import React from 'react';
-import { Button } from '../Button4';
+import { Button } from '../Button5';
 
 describe('Button', () => {
     it('matches snapshot', () => {
         const component = renderer.create((
             <Button />
-            //
         ))
         expect(component.toJSON()).toMatchSnapshot()
     })
